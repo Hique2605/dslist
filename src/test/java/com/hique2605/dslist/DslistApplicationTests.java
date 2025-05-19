8d8694b81105c3e3a1a765cb5d34dc42c9513c57
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-19 13:36:54 -0300

    Project Created

---
+++ src/test/java/com/hique2605/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hique2605.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
