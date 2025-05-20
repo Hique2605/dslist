e507cf57997355ea70ad1f25141bcd4649ad98cf
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-20 11:33:25 -0300

    GET game by id

--- src/main/java/com/hique2605/dslist/controllers/GameController.java
+++ src/main/java/com/hique2605/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hique2605.dslist.dto.GameDTO;
 import com.hique2605.dslist.dto.GameMinDTO;
 import com.hique2605.dslist.services.GameService;
 
@@ -16,13 +18,19 @@ public class GameController	{
 	
 	@Autowired
 	private GameService gameService;
-
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
-
+	
 	
 	
 	
